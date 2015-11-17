319be0076cc2d34eb2d1c2271c90127107280faf
Hinell <al.neodim@gmail.com>
2015-11-17 18:32:49 +0300

    Improved tests for single case

--- index.test.js
+++ index.test.js
@@ -1,9 +1,11 @@
+/* Todo: tests for non-async targets */
+
 expect      = require('expect.js');
-oath        = require('./index.js');
-describe('Oath modulest tests'  , function () {
-  describe('it works fine with' ,function(){
+Oath        = require('./index.js');
+describe('Oath modules tests'  , function () {
+  describe('it works fine with' ,function() {
     it('common.js modules', function () {
-      expect(oath).to.be.an('function')
+      expect(Oath).to.be.an('function')
     });
     it('browser\'s global', function () {
       new Function(''
@@ -17,70 +19,33 @@ describe('Oath modulest tests'  , function () {
     it('AMD modules')
   });
 });
+describe('Creation of the new Oath (Promise) object with', function () {
+  describe('a single postpone parameter: '  , function () {
+    it('passes resolved value into the first then()\'s handler which should return value into the next and so on'   , function(done) {
+      new Oath(function (resolve) { setTimeout(function () { resolve('foo') },100) })
+          .then(function (foo) { expect(foo).to.eql('foo'); return 'bar'})
+          .then(function (bar) { expect(bar).to.eql('bar'); done()})
+    });
+    it('passes rejected value into the catch handler which provides return as parameter of the next then() handler and so on' , function(done) {
+      new Oath(function (resolve,reject) { setTimeout(function () { reject(new Error('error'))},100) })
+          .catch(function () { done(new Error('Expected first catch() handler to be not called because it should be reassigned by second!'))})
+          .catch(function (error) { console.log(error[0] instanceof Error); expect(error.message).to.eql('error'); return 'catchReturn'})
+          .then (function (catchResult) { expect(catchResult).to.eql('catchReturn');done() })
+    });
 
-Oath      = require('./index.js');
-describe('Creating of the new Oath object with', function () {
-  describe('a single targeted postpone call', function () {
-    it('resolves promise by passing result into then() handler' ,function () {
-      new Oath(function (resolve) {
-        setTimeout(function () { resolve('ok') },100)
-      }).then(function (ok) {
-        expect(ok).to.eql('ok')
-      })
+    it('not calls catch()\'s handler if promise is already resolved'  , function(done){
+      new Oath(function (resolve,reject){ setTimeout(function () { resolve('ok') },100) })
+          .catch(function () { done(new Error('Expected catch() handler to be not called!'))})
+          .then (function () { done()})
     });
-    it('reassigns then() handler by each invocation of then()'  ,function (done) {
-      new Oath(function (resolve) {
-        setTimeout(function () { resolve('ok') },100)
-      }).then(function () {
-        done(new Error('Expected then() handler to be not called!'))
-      }).then(function (ok) {
-        expect(ok).to.eql('ok');
-        done()
-      })
-    });
-    it('rejects promise by an Error argument and passes its instance into catch() handler',function(){
-      new Oath(function (resolve,reject) {
-        setTimeout(function () {
-          reject(new Error('error'));
-          },100)
-      })
-      .catch(function (error) {
-        expect(error.message).to.eql('error')
-      })
-    });
-    it('not calls catch()\'s handler if promise is resolved' ,function(done){
-      new Oath(function (resolve,reject) {
-        setTimeout(function () { resolve('ok') },100)
-      }).then (function () { done()})
-        .catch(function () { done(new Error('Expected catch() handler to be not called!'))})
-    });
-    it('not calls then()\'s handler if promise is rejected'  ,function(done){
-      new Oath(function (resolve,reject) {
-        setTimeout(function () { reject('fail') },100)
-      }).catch  (function () { done() })
-        .then   (function () { done(new Error('Expected then() handler to be not called if promise was rejected ')) })
-    });
-    it('chaines then()\'s  handlers returns after the promise is resolved',function(done){
-      new Oath(function (resolve,reject) {
-       setTimeout(function () { resolve('ok') },100)
-      }).then(function (ok) {
-        ok || expect().fail('Expected the promise passing a resolved value into the first nearest then() handler');
-        return {ok:ok}
-      }).then(function (obj) {
-         if (!obj.ok) expect().fail('Expected a value to be a result of return of the first then() handler');
-        done()
-      })
-    });
-    it('chaines catch()\'s handlers returns after the promise is rejected',function(done){
-      new Oath(function (resolve,reject) {
-       setTimeout(function () { reject('fail') },100)
-      }).catch(function (fail) {
-          fail || done(new Error('Expected an rejected value'));
-          return {result:'result of a catching'}
-      }).then(function (obj) {
-        obj.result || done(new Error('Expected a value to be passed into the then() handler retrieved by the catch\'s handler return'));
-        done()
-      })
+    it('not calls first then()\'s handler if promise was rejected'    , function(done){
+      new Oath(function (resolve,reject) { setTimeout(function () { reject('fail') },100) })
+          .then (function () { done(new Error('Expected first then()\'s handler to be not called if promise was rejected ')) })
+          .catch(function () { return 'foo' })
+          .then (function (foo) { expect(foo).to.eql('foo'); done() })
     });
   });
+  describe('a several parameters: '         , function () {
+    it.skip('fires then()\'s handlers with result provided by target resolving', function () {  })
+  })
 });
